a74f0f633cc2812b92b6449e421eb343679221c7
Lachlan Newman <lachnewman007@gmail.com>
2017-06-27 20:24:04 +0800

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $('.nav').on('click', function () {
+        if ($('.navbar-toggle').css('display') != 'none') {
+            $(".navbar-toggle").trigger("click");
+        }
+    });
+});
